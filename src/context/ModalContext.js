aafff52d4bf9704178f9f6e121531baeab95e391
vamshi9666 <vamshi9666@riseup.net>
2020-01-28 00:41:10 +0530

    woe

--- src/context/ModalContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const ModalContext = createContext({
-  component: null,
-  props: {},
-  openModal: () => ({}),
-  hideModal: () => ({})
-});
-export default ModalContext;
